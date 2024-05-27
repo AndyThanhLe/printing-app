8b9ee200c09a4bde6553fee77a120291abd25cb2
Andy <49623329+AndyThanhLe@users.noreply.github.com>
2024-05-27 18:49:25 -0400

    rudimentary file upload

--- routes/estimator.js
+++ routes/estimator.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const upload = multer({ dest: 'temp/' });
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('estimator', { title: 'Estimator' });
+});
+
+
+/* form submission page. */
+router.post('/upload', upload.single('stl_file'), function(req, res, next) {
+  console.log(req.file, req.body);
+  res.json({ message: 'File uploaded successfully!' });
 });
 
 module.exports = router;
