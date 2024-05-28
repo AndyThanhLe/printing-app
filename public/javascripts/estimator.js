1e29ee61ca534a265627855c9f95023079387d4d
Andy <49623329+AndyThanhLe@users.noreply.github.com>
2024-05-28 17:21:30 -0400

    file change without a selected file no longer submits form

--- public/javascripts/estimator.js
+++ public/javascripts/estimator.js
@@ -1,2 +1,10 @@
 document.getElementById('stl-import').addEventListener('click', () => { document.getElementById('file-submission').click(); });
-document.getElementById('file-submission').addEventListener('change', () => { document.getElementById('import-form').submit();  });
+document.getElementById('file-submission').addEventListener('change', submitFile);
+
+function submitFile() {
+    if (document.getElementById('file-submission').files.length > 0) {
+        document.getElementById('import-form').submit();
+    }
+}
+
+
