7103f88024a1baa98e9837f03c8641fd210d46bf
Andy <49623329+AndyThanhLe@users.noreply.github.com>
2024-05-28 16:47:49 -0400

    renamed js script that handles three; prettier button to import STL

--- routes/estimator.js
+++ routes/estimator.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const multer = require('multer');
-const upload = multer({ dest: 'temp/' });
 
+// Multer setup
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/');
+  }, 
+  filename: function (req, file, cb) {
+    cb(null, `${file.originalname}`);
+  }
+});
+const upload = multer({ 
+  storage: storage,
+  limits: { fileSize: 100000000 },
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,7 +22,7 @@ router.get('/', function(req, res, next) {
 });
 
 
-/* form submission page. */
+/* File submission */
 router.post('/upload', upload.single('stl_file'), function(req, res, next) {
   console.log(req.file, req.body);
   res.json({ message: 'File uploaded successfully!' });
