09f54eec7ff01fe8ae1501724062b742231b092a
Andy Le <andythanhle2000@gmail.com>
2024-05-29 03:20:45 -0400

    revise indentation of files

--- public/javascripts/estimator.js
+++ public/javascripts/estimator.js
@@ -3,32 +3,32 @@ document.getElementById('stl-import').addEventListener('click', () => { document
 document.getElementById('file-submission').addEventListener('change', upload);
 
 async function upload() {
-    if (document.getElementById('file-submission').files.length == 0) {
-        return;
-    }
+  if (document.getElementById('file-submission').files.length == 0) {
+    return;
+  }
 
-    console.log(document.getElementById('file-submission').files);
+  console.log(document.getElementById('file-submission').files);
 
-    const formData = new FormData();
-    formData.append('stl_file', document.getElementById('file-submission').files[0]);
+  const formData = new FormData();
+  formData.append('stl_file', document.getElementById('file-submission').files[0]);
 
-    const response = await fetch('./upload', {
-        method: 'POST',
-        body: formData,
+  const response = await fetch('./upload', {
+    method: 'POST',
+    body: formData,
+  });
+
+  console.log(response);
+  response.json()
+    .then((r) => {
+      console.log(r);
     });
 
-    console.log(response);
-    response.json()
-        .then((r) => {
-            console.log(r);
-        });
+  // r.then((res) => {
+  //   console.log(res);
+  // });
 
-    // r.then((res) => {
-    //     console.log(res);
-    // });
-
-    // await fetch('./upload', {
-    //     method: 'POST',
-    //     body: document.getElementById('file-submission').files[0],
-    // });
+  // await fetch('./upload', {
+  //   method: 'POST',
+  //   body: document.getElementById('file-submission').files[0],
+  // });
 }
--- public/javascripts/three-preview.js
+++ public/javascripts/three-preview.js
@@ -1,102 +1,98 @@
-
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader';
 import WebGL from 'three/addons/capabilities/WebGL.js';
 
-
 const container = document.getElementById('preview');
 let camera, renderer, scene;
 let controls;
 
-
 init();
 
 if (WebGL.isWebGLAvailable()) {
-    animate();
+  animate();
 }
 else {
-    const warning = WebGL.getWebGLErrorMessage();
-    document.body.appendChild(warning);
+  const warning = WebGL.getWebGLErrorMessage();
+  document.body.appendChild(warning);
 }
 
-
 function init() {
-    // Camera
-    camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
-    camera.position.set(4, 2, 4);
+  // Camera
+  camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
+  camera.position.set(4, 2, 4);
 
-    // Renderer
-    renderer = new THREE.WebGLRenderer();
+  // Renderer
+  renderer = new THREE.WebGLRenderer();
 
-    renderer.setSize(container.clientWidth, container.clientHeight, false);
+  renderer.setSize(container.clientWidth, container.clientHeight, false);
 
-    controls = new OrbitControls(camera, renderer.domElement);
-    controls.enableDamping = true;
-    controls.update();
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.enableDamping = true;
+  controls.update();
 
-    // Scene
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color( 0xa0a0a0 );
-    scene.fog = new THREE.Fog( 0xa0a0a0, 4, 20 );
-    scene.add(new THREE.AxesHelper(50));
-
-
-    // Lights
-    const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444, 3 );
-    hemiLight.position.set( 0, 20, 0 );
-    scene.add( hemiLight );
-
-    const directionalLight = new THREE.DirectionalLight( 0xffffff, 3 );
-    directionalLight.position.set( 0, 20, 10 );
-    directionalLight.castShadow = true;
-    directionalLight.shadow.camera.top = 2;
-    directionalLight.shadow.camera.bottom = - 2;
-    directionalLight.shadow.camera.left = - 2;
-    directionalLight.shadow.camera.right = 2;
-    scene.add( directionalLight );
+  // Scene
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color( 0xa0a0a0 );
+  scene.fog = new THREE.Fog( 0xa0a0a0, 4, 20 );
+  scene.add(new THREE.AxesHelper(50));
 
 
-    // Ground
-    const ground = new THREE.Mesh( new THREE.PlaneGeometry( 40, 40 ), new THREE.MeshPhongMaterial( { color: 0xbbbbbb, depthWrite: false } ) );
-    ground.rotation.x = - Math.PI / 2;
-    ground.receiveShadow = true;
-    scene.add( ground );
+  // Lights
+  const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444, 3 );
+  hemiLight.position.set( 0, 20, 0 );
+  scene.add( hemiLight );
 
-    const grid = new THREE.GridHelper( 40, 20, 0x000000, 0x000000 );
-    grid.material.opacity = 0.2;
-    grid.material.transparent = true;
-    scene.add( grid );
+  const directionalLight = new THREE.DirectionalLight( 0xffffff, 3 );
+  directionalLight.position.set( 0, 20, 10 );
+  directionalLight.castShadow = true;
+  directionalLight.shadow.camera.top = 2;
+  directionalLight.shadow.camera.bottom = - 2;
+  directionalLight.shadow.camera.left = - 2;
+  directionalLight.shadow.camera.right = 2;
+  scene.add( directionalLight );
 
 
-    // Load STL
-    const loader = new STLLoader();
-    loader.load( '../models/3DBenchy.stl', function ( geometry ) {
-        const material = new THREE.MeshPhongMaterial( { color: 0xff9c7c, specular: 0x494949, shininess: 200 } );
-        const mesh = new THREE.Mesh( geometry, material );
+  // Ground
+  const ground = new THREE.Mesh( new THREE.PlaneGeometry( 40, 40 ), new THREE.MeshPhongMaterial( { color: 0xbbbbbb, depthWrite: false } ) );
+  ground.rotation.x = - Math.PI / 2;
+  ground.receiveShadow = true;
+  scene.add( ground );
 
-        mesh.scale.set(0.05, 0.05, 0.05);
-        mesh.rotateY(- Math.PI / 2);
-        mesh.rotateX(- Math.PI / 2);
-        
-        mesh.castShadow = true;
-        mesh.receiveShadow = true;
-        scene.add( mesh );
-    });
+  const grid = new THREE.GridHelper( 40, 20, 0x000000, 0x000000 );
+  grid.material.opacity = 0.2;
+  grid.material.transparent = true;
+  scene.add( grid );
 
-    window.addEventListener('resize', onWindowResize);
-    container.appendChild(renderer.domElement);
+
+  // Load STL
+  const loader = new STLLoader();
+  loader.load( '../models/3DBenchy.stl', function ( geometry ) {
+    const material = new THREE.MeshPhongMaterial( { color: 0xff9c7c, specular: 0x494949, shininess: 200 } );
+    const mesh = new THREE.Mesh( geometry, material );
+
+    mesh.scale.set(0.05, 0.05, 0.05);
+    mesh.rotateY(- Math.PI / 2);
+    mesh.rotateX(- Math.PI / 2);
+    
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+    scene.add( mesh );
+  });
+
+  window.addEventListener('resize', onWindowResize);
+  container.appendChild(renderer.domElement);
 }
 
 
 function onWindowResize() {
-    camera.aspect = container.clientWidth / container.clientHeight;
-    camera.updateProjectionMatrix();
-    renderer.setSize(container.clientWidth, container.clientHeight, false);
+  camera.aspect = container.clientWidth / container.clientHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(container.clientWidth, container.clientHeight, false);
 }
 
 
 function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
 }
