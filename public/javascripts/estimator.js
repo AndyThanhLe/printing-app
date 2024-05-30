8fd2d5111e56389eef96b5223b7bd34adb34ef05
Andy Le <andythanhle2000@gmail.com>
2024-05-29 23:52:57 -0400

    rudimentary error handling

--- public/javascripts/estimator.js
+++ public/javascripts/estimator.js
@@ -11,17 +11,28 @@ async function upload() {
   const formData = new FormData();
   formData.append('stl-file', document.getElementById('file-submission').files[0]);
 
-  const response = await fetch(`${window.location.pathname}/upload`, {
-    method: 'POST',
-    body: formData,
-  });
-
-  console.log(response);
-  response.json()
-    .then((r) => {
-      console.log(r);
-    })
-    .catch((r) => {
-      console.log(r)
+  try {
+    const response = await fetch(`${window.location.pathname}/upload`, {
+      method: 'POST',
+      body: formData,
     });
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+  
+    console.log(response);
+    response.json()
+      .then((r) => {
+        console.log(r);
+      })
+      .catch((r) => {
+        console.log(r)
+      });
+  }
+  catch (error) {
+    console.error(`An error has occurred!: ${error}`);
+  }
+
+  // Populate
 }
