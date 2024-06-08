f179fd17ff8cf2030c4815946a8aa34c30c82c3e
Andy Le <andythanhle2000@gmail.com>
2024-06-08 17:45:53 -0400

    revise scale

--- public/javascripts/three-preview.js
+++ public/javascripts/three-preview.js
@@ -33,7 +33,7 @@ function animate() {
 function init() {
   // Camera
   camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000);
-  camera.position.set(4, 2, 4);
+  camera.position.set(200, 150, 200);
 
   // Renderer
   renderer = new THREE.WebGLRenderer();
@@ -47,17 +47,17 @@ function init() {
   // Scene
   scene = new THREE.Scene();
   scene.background = new THREE.Color( 0xa0a0a0 );
-  scene.fog = new THREE.Fog( 0xa0a0a0, 4, 20 );
-  scene.add(new THREE.AxesHelper(50));
+  scene.fog = new THREE.Fog( 0xa0a0a0, 0, 2000 );
+  // scene.add(new THREE.AxesHelper(50));
 
 
   // Lights
   const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444, 3 );
-  hemiLight.position.set( 0, 20, 0 );
+  hemiLight.position.set( 0, 200, 0 );
   scene.add( hemiLight );
 
   const directionalLight = new THREE.DirectionalLight( 0xffffff, 3 );
-  directionalLight.position.set( 0, 20, 10 );
+  directionalLight.position.set( 0, 200, 10 );
   directionalLight.castShadow = true;
   directionalLight.shadow.camera.top = 2;
   directionalLight.shadow.camera.bottom = - 2;
@@ -67,12 +67,12 @@ function init() {
 
 
   // Ground
-  const ground = new THREE.Mesh( new THREE.PlaneGeometry( 40, 40 ), new THREE.MeshPhongMaterial( { color: 0xbbbbbb, depthWrite: false } ) );
+  const ground = new THREE.Mesh( new THREE.PlaneGeometry( 256, 256 ), new THREE.MeshPhongMaterial( { color: 0xbbbbbb, depthWrite: false } ) );
   ground.rotation.x = - Math.PI / 2;
   ground.receiveShadow = true;
   scene.add( ground );
 
-  const grid = new THREE.GridHelper( 40, 20, 0x000000, 0x000000 );
+  const grid = new THREE.GridHelper( 256, 16, 0x000000, 0x000000 );
   grid.material.opacity = 0.2;
   grid.material.transparent = true;
   scene.add( grid );
@@ -105,9 +105,9 @@ export function loadSTL(fileName) {
     mesh = new THREE.Mesh( geometry, material );
 
     // TODO: Scale and rotate accordingly
-    mesh.scale.set(0.05, 0.05, 0.05);
-    mesh.rotateY(- Math.PI / 2);
-    mesh.rotateX(- Math.PI / 2);
+    // mesh.scale.set(0.05, 0.05, 0.05);
+    // mesh.rotateY(- Math.PI / 2);
+    // mesh.rotateX(- Math.PI / 2);
     
     mesh.castShadow = true;
     mesh.receiveShadow = true;
