4b432c418873c27015490b6020e2cd9875775b2d
Andy Le <andythanhle2000@gmail.com>
2024-06-19 16:00:19 -0400

    start implementation of cart

--- public/scripts/cart.ts
+++ public/scripts/cart.ts
@@ -1,1 +1,23 @@
-// TODO: Implement...
+window.onload = () => {
+    console.log(JSON.parse(sessionStorage.getItem('cart')));
+    loadCart();
+};
+
+
+function loadCart() {
+    const cart = JSON.parse(sessionStorage.getItem('cart'));
+    const items = document.getElementById('items') as HTMLDivElement;
+    
+    for (const key in cart) {
+        let card = document.createElement('div');
+        card.className = 'panel-content';
+
+        for (const attr in cart[key]) {
+            let p = document.createElement('p');
+            p.innerText = `${attr}: ${cart[key][attr]}`;
+            card.appendChild(p);
+        }
+
+        items.appendChild(card);
+    }
+}
--- routes/cart.js
+++ routes/cart.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET cart page. */
 router.get('/', function(req, res, next) {
   console.log(`The session id is: ${req.session.userId}`);
-  res.render('cart', { title: 'cart' });
+  res.render('cart', { title: 'Cart' });
 });
 
 module.exports = router;
